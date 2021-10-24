a3deb98243b49b73c6ef5c364884fbc106a9e3f8
OlgaMikhaylyukova <olele@inbox.ru>
2021-10-24 14:07:48 +0300

    Added tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -1,25 +1,25 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-
-class MobileBankApiTestV1 {
+public class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
         given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
+                .baseUri("http://0.0.0.0:9999/api/v1")
                 .when()
                 .get("/demo/accounts")
-                // Проверки
                 .then()
                 .statusCode(200)
-                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .header("Content-Length", "433")
+                .header("Connection", "keep-alive")
+                .contentType(ContentType.JSON)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
